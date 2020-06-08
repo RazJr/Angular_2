0364d5a3f533d0e37ada64a9869721e451e89677
RazJr <minirazjr@outlook.com>
2020-06-08 11:09:59 -0500

    error

---
+++ Practica_5/TemasComplementarios/src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styles: [
+  ]
+})
+export class PipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
